09b6cc805a34132416b518e87a9d3cc6a171b954
ixiaohei <ixiaohei@126.com>
2013-09-28 00:30:40 +0800

    mdofy some bug and improve the user experience

--- src/js/init.js
+++ src/js/init.js
@@ -1,7 +1,15 @@
 $(function(){
-    //init();
-    //alert($("title").html());
+    console.info($("title").html());
     var imageURL = chrome.extension.getURL("image/gotop.gif");
-    $("body").prepend("<a id='cont_a' href='#head_a'><img id='contIcon' src='" + imageURL +"' /></a>");
-    $("body").prepend("<div id='head_a'/>");
+    $("body").prepend("<a id='cont_a123456789' href='#head_a123456789'><img id='contIcon' src='" + imageURL +"' /></a>");
+    $("body").prepend("<div id='head_a123456789'/>");
+    $(window).scroll(function(){
+    	if($(document.body).scrollTop() > ($(window).height()*0.1)) {
+            console.debug("body.scrollTop:" + $(document.body).scrollTop() + "px window.height*0.1:" + ($(window).height()*0.1));
+    		$("#cont_a123456789").fadeIn(1000);
+    	}else if($(document.body).scrollTop() <= ($(window).height()*0.1)) {
+            console.debug("body.scrollTop:" + $(document.body).scrollTop() + "px window.height*0.1:" + ($(window).height()*0.1))
+    		$("#cont_a123456789").fadeOut(1000);
+    	}
+    });
 });
